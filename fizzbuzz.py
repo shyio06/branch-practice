c539de6bcbcfc98cedc88bf840a36eb9dfab0ae9
shyio06 <lilly9017@naver.com>
2025-04-23 11:41:26 +0900

    feat: Rebase

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,4 @@
-# Create fizzbuzz with 1 if line
-<<<<<<< HEAD
-for i in range(1,5+1):
-=======
 for i in range(1,20+1):
->>>>>>> 49468a9 (feat: Adjust endnum to 5)
     if i % 3 == 0 or i % 5 == 0:
         print('Fizz' * (i % 3 == 0) + 'Buzz' * (i % 5 == 0))
     else:
